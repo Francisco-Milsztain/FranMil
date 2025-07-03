1ec655feeb6476ed3d6f467504055cf5781090a9
Francisco-Mil <francisco.tmilsztain@gmail.com>
2025-07-03 01:05:51 -0300

    Add files via upload

---
+++ flutter_provider_agregar_eliminar_lista/lib/providers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:flutter_provider_agregar_a_lista/entities/loginstate.dart';
+import 'package:flutter_provider_agregar_a_lista/entities/userdata.dart';
+import 'package:flutter_provider_agregar_a_lista/entities/listofgames.dart';
+import 'package:flutter_provider_agregar_a_lista/entities/selecteduser.dart';
+
+final variablesProvider = StateProvider<List<LoginState>>((ref) => listaVariables);
+final userdataProvider = StateProvider<List<Users>>((ref) => listaUsuarios);
+final selecteduserProvider = StateProvider<List<Selected>>((ref) => selectedlist);
+final listaJuegosProvider = StateProvider<List<Games>>((ref) => listaJuegos);
+final selectedgameProvider = StateProvider<Games?>((ref) => null);
